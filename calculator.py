00857e3696da59da708d772aac8fa3cbfce33b0f
Tpallavi2610 <145150171+Tpallavi2610@users.noreply.github.com>
2023-09-15 22:53:25 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,54 @@
+from tkinter import*
+root=Tk()
+root.title("Simple Calculator")
+root.geometry("570x600+100+200")
+root.resizable(False,True)
+root.configure(bg="black")
+equation = ""
+def show(value):
+    global equation
+    equation+=value
+    lbl_result.config(text=equation)
+def clear():
+    global equation
+    equation = ""
+    lbl_result.config(text=equation)
+def calculate():
+    global equation
+    result = ""
+    if equation != "":
+        try:
+            result=eval(equation)
+        except:
+            result="error"
+            equation = ""
+    lbl_result.config(text=result)
+
+lbl_result=Label(root,width=25,height=2,text="",font=("arial",30))
+lbl_result.pack()
+
+b1=Button(root,text="C",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#3697f5",command=lambda:clear()).place(x=10,y=100)
+b2=Button(root,text="/",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("/")).place(x=150,y=100)          
+b3=Button(root,text="%",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("%")).place(x=290,y=100)          
+b4=Button(root,text="*",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("*")).place(x=430,y=100)
+
+b5=Button(root,text="7",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("7")).place(x=10,y=200)
+b6=Button(root,text="8",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("8")).place(x=150,y=200)          
+b7=Button(root,text="9",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("9")).place(x=290,y=200)
+b8=Button(root,text="-",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("-")).place(x=430,y=200)
+
+b9=Button(root,text="4",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("4")).place(x=10,y=300)
+b10=Button(root,text="5",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("5")).place(x=150,y=300)          
+b11=Button(root,text="6",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("6")).place(x=290,y=300)
+b12=Button(root,text="+",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("+")).place(x=430,y=300)
+
+b13=Button(root,text="1",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("1")).place(x=10,y=400)
+b14=Button(root,text="2",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("2")).place(x=150,y=400)          
+b15=Button(root,text="3",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("3")).place(x=290,y=400)
+b16=Button(root,text="0",width=11,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show("0")).place(x=10,y=500)
+
+
+Button(root,text=".",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda:show(".")).place(x=290,y=500)
+Button(root,text="=",width=5,height=3,font=("arial",30,"bold"),fg="#fff",bg="#fe9037",command=lambda:calculate()).place(x=430,y=400)
+
+root.mainloop
